3800e67631739f6d2b09a44edeef7f1cf6654ff2
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-07-08 10:50:20 +0530

    FSM: change code to manage multiple property details

--- municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
@@ -49,63 +49,66 @@ public class PropertyService {
 				new TypeReference<Map<String, Object>>() {
 				});
 
-		Map<String, Object> propertyDetails = ((List<Map<String, Object>>) propertySearchResponse
-				.get("GETPROPDETAILSResult")).get(0);
+		List<Map<String, Object>> propertyDetailsList = ((List<Map<String, Object>>) propertySearchResponse
+				.get("GETPROPDETAILSResult"));
 
-		log.info("RMC propertyDetails: " + propertyDetails.toString());
+		log.info("RMC propertyDetailsList: " + propertyDetailsList.toString());
 //			Property currentProperty = new Property();
 
-		Object area = propertyDetails.get("TOTAL_PLOT_AREA");
-		Double landArea = 0.0;
-		if (area == null) {
-			landArea = null;
-		} else {
-			landArea = Double.valueOf(area.toString());
-		}
-		Object wardNo = propertyDetails.get("WARD_NO");
-		Object wardName = propertyDetails.get("WARD_NAME");
-		Object zoneNO = propertyDetails.get("ZONE_NO");
-		Locality locality = Locality.builder().code(wardNo != null ? "RMCLC" + wardNo.toString() : null)
-				.name(wardName != null ? wardName.toString() : null).label("Locality").build();
+		for (Map<String, Object> propertyDetails : propertyDetailsList) {
+			Object area = propertyDetails.get("TOTAL_PLOT_AREA");
+			Double landArea = 0.0;
+			if (area == null) {
+				landArea = null;
+			} else {
+				landArea = Double.valueOf(area.toString());
+			}
+			Object wardNo = propertyDetails.get("WARD_NO");
+			Object wardName = propertyDetails.get("WARD_NAME");
+			Object zoneNO = propertyDetails.get("ZONE_NO");
+			Locality locality = Locality.builder().code(wardNo != null ? "RMCLC" + wardNo.toString() : null)
+					.name(wardName != null ? wardName.toString() : null).label("Locality").build();
 
-		Map<String, Object> addressAdditional = new HashMap<>();
+			Map<String, Object> addressAdditional = new HashMap<>();
 
-		addressAdditional.put("colonyName", propertyDetails.get("COLONY_NAME"));
-		addressAdditional.put("houseNo", propertyDetails.get("HOUSE_NO"));
-		addressAdditional.put("wardNo", propertyDetails.get("WARD_NO"));
-		addressAdditional.put("address", propertyDetails.get("ADDRESS"));
-		addressAdditional.put("zoneNo", propertyDetails.get("ZONE_NO"));
+			addressAdditional.put("colonyName", propertyDetails.get("COLONY_NAME"));
+			addressAdditional.put("houseNo", propertyDetails.get("HOUSE_NO"));
+			addressAdditional.put("wardNo", propertyDetails.get("WARD_NO"));
+			addressAdditional.put("address", propertyDetails.get("ADDRESS"));
+			addressAdditional.put("zoneNo", propertyDetails.get("ZONE_NO"));
 
-		Address address = Address.builder().city("Raipur").tenantId("cg.raipur")
-				.wardNo(wardNo != null ? wardNo.toString() : null).zoneNo(wardNo != null ? zoneNO.toString() : null)
-				.locality(locality).build();
+			Address address = Address.builder().city("Raipur").tenantId("cg.raipur")
+					.wardNo(wardNo != null ? wardNo.toString() : null).zoneNo(wardNo != null ? zoneNO.toString() : null)
+					.locality(locality).build();
 
-		List<String> owners = Arrays.asList(propertyDetails.get("PROPERTY_OWNER").toString().split(","));
-		String ownershipcategory = owners.size() == 1 ? "INDIVIDUAL.SINGLEOWNER" : "INDIVIDUAL.MULTIPLEOWNERS";
+			List<String> owners = Arrays.asList(propertyDetails.get("PROPERTY_OWNER").toString().split(","));
+			String ownershipcategory = owners.size() == 1 ? "INDIVIDUAL.SINGLEOWNER" : "INDIVIDUAL.MULTIPLEOWNERS";
 
-		Object coveredArea = propertyDetails.get("TOTAL_COVERED_AREA");
-		BigDecimal superBuiltUpArea = coveredArea != null ? new BigDecimal(coveredArea.toString()) : BigDecimal.ZERO;
-		Long noOfFloors = propertyDetails.get("TOTAL_FLOORS") != null
-				? ((Number) propertyDetails.get("TOTAL_FLOORS")).longValue()
-				: null;
+			Object coveredArea = propertyDetails.get("TOTAL_COVERED_AREA");
+			BigDecimal superBuiltUpArea = coveredArea != null ? new BigDecimal(coveredArea.toString())
+					: BigDecimal.ZERO;
+			Long noOfFloors = propertyDetails.get("TOTAL_FLOORS") != null
+					? ((Number) propertyDetails.get("TOTAL_FLOORS")).longValue()
+					: null;
 
-		Property currentProperty = Property.builder().source(Source.fromValue("MUNICIPAL_RECORDS"))
-				.ownershipCategory(ownershipcategory).channel(Channel.fromValue("SYSTEM"))
-				.superBuiltUpArea(superBuiltUpArea).usageCategory(propertyDetails.get("PROPERTY_USE").toString())
+			Property currentProperty = Property.builder().source(Source.fromValue("MUNICIPAL_RECORDS"))
+					.ownershipCategory(ownershipcategory).channel(Channel.fromValue("SYSTEM"))
+					.superBuiltUpArea(superBuiltUpArea).usageCategory(propertyDetails.get("PROPERTY_USE").toString())
 //					.propertyType(rs.getString("propertytype"))
-				.noOfFloors(noOfFloors)
+					.noOfFloors(noOfFloors)
 //					.auditDetails(auditdetails)
-				.landArea(landArea).address(address).propertyId(propertyDetails.get("PROP_UID").toString())
-				.tenantId("cg.raipur").status(Status.fromValue("ACTIVE"))
-				.dueAmount(propertyDetails.get("DUE_AMOUNT").toString()).build();
+					.landArea(landArea).address(address).propertyId(propertyDetails.get("PROP_UID").toString())
+					.tenantId("cg.raipur").status(Status.fromValue("ACTIVE"))
+					.dueAmount(propertyDetails.get("DUE_AMOUNT").toString()).build();
 
-		OwnerInfo owner = OwnerInfo.builder().status(Status.fromValue("ACTIVE")).tenantId("cg.raipur")
-				.mobileNumber(propertyDetails.get("MOBILE").toString())
-				.name(propertyDetails.get("PROPERTY_OWNER").toString().split(",")[0]).build();
+			OwnerInfo owner = OwnerInfo.builder().status(Status.fromValue("ACTIVE")).tenantId("cg.raipur")
+					.mobileNumber(propertyDetails.get("MOBILE").toString())
+					.name(propertyDetails.get("PROPERTY_OWNER").toString().split(",")[0]).build();
 
-		currentProperty.addOwnersItem(owner);
+			currentProperty.addOwnersItem(owner);
 
-		properties.add(currentProperty);
+			properties.add(currentProperty);
+		}
 
 //		} catch (Exception ex) {
 //			log.error("Error : " + ex.toString());
