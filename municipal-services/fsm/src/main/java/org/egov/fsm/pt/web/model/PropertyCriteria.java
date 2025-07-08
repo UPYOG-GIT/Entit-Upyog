20871fcb2ea21721277768effdfa63271bd419db
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-07-08 15:46:27 +0530

    FSM: change propertyId from Set to String

--- municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/model/PropertyCriteria.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/model/PropertyCriteria.java
@@ -21,7 +21,8 @@ public class PropertyCriteria {
 
 	private String tenantId;
 
-	private Set<String> propertyIds;
+//	private Set<String> propertyIds;
+	private String propertyId;
 
 	private Set<String> tenantIds;
 	
--- municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
@@ -34,8 +34,9 @@ public class PropertyService {
 	private RestTemplate restTemplate;
 
 	public List<Property> getPropertiesById(PropertyCriteria criteria) throws Exception {
-		String propertyId = (criteria.getPropertyIds().isEmpty() || criteria.getPropertyIds().size() == 0) ? null
-				: criteria.getPropertyIds().iterator().next();
+//		String propertyId = (criteria.getPropertyId() ) ? null
+//				: criteria.getPropertyId();
+		String propertyId = criteria.getPropertyId();
 		String mobileNumber = criteria.getMobileNumber();
 
 		String propertyUrl = "";
