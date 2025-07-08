e2fb1c30b04c482c85991ed5964b3354a98e1403
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-07-08 14:37:11 +0530

    FSM: add mobile number search URL

--- municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
@@ -35,15 +35,14 @@ public class PropertyService {
 
 	public List<Property> getPropertiesById(PropertyCriteria criteria) throws Exception {
 		String propertyId = criteria.getPropertyIds().isEmpty() ? null : criteria.getPropertyIds().iterator().next();
-        String mobileno =  criteria.getMobileNumber();
-        
-        String propertyUrl=""; 
-        if(propertyId != null) {
-		     propertyUrl = "https://mcraipur.in/api/getPropertyDetails?PROP_UID=" + propertyId;
-        }else if(propertyId == null && mobileno != null)
-        {
-        	 propertyUrl = "https://apis.mcraipur.in/api/searchPropertyList?MOBILE=" + mobileno;
-        }
+		String mobileNumber = criteria.getMobileNumber();
+
+		String propertyUrl = "";
+		if (propertyId != null) {
+			propertyUrl = "https://mcraipur.in/api/getPropertyDetails?PROP_UID=" + propertyId;
+		} else if (mobileNumber != null) {
+			propertyUrl = "https://apis.mcraipur.in/api/searchPropertyList?MOBILE=" + mobileNumber;
+		}
 
 		List<Property> properties = new ArrayList<>();
 
