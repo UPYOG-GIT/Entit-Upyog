f51a0ed448d61fda8170e74c7091c82fc38e951b
ravikantentit <kantravi3599@gmail.com>
2025-07-08 17:02:34 +0530

    FSM CHanges for mobile property search

--- municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
@@ -34,10 +34,36 @@ public class PropertyService {
 	private RestTemplate restTemplate;
 
 	public List<Property> getPropertiesById(PropertyCriteria criteria) throws Exception {
-//		String propertyId = (criteria.getPropertyId() ) ? null
-//				: criteria.getPropertyId();
+
 		String propertyId = criteria.getPropertyId();
-//		String mobileNumber = criteria.getMobileNumber();
+		String mobileNumber = criteria.getMobileNumber();
+
+		String propertyUrl;
+		List<Property> properties = new ArrayList<>();
+
+		if (propertyId != null) {
+			properties = searchPropertyResult(propertyId);
+		} else if (mobileNumber != null) {
+			propertyUrl = propertyUrl = "https://apis.mcraipur.in/api/searchPropertyList?MOBILE=" + mobileNumber;
+			String response = restTemplate.getForObject(propertyUrl, String.class);
+			log.info("RMC response Mobile : " + response);
+			ObjectMapper objectMapper = new ObjectMapper();
+			Map<String, Object> propertySearchResponse = objectMapper.readValue(response,
+					new TypeReference<Map<String, Object>>() {
+					});
+
+			List<Map<String, Object>> propertyDetails = (List<Map<String, Object>>) propertySearchResponse
+					.get("SearchPropertyResult");
+			for (Map<String, Object> details : propertyDetails) {
+				String propertyUid = details.get("PROPERTY_UID").toString();
+				properties = searchPropertyResult(propertyId);
+			}
+		}
+
+		return properties;
+	}
+
+	public List<Property> searchPropertyResult(String propertyId) throws Exception {
 
 		String propertyUrl = "https://mcraipur.in/api/getPropertyDetails?PROP_UID=" + propertyId;
 		/*
@@ -116,16 +142,99 @@ public class PropertyService {
 		currentProperty.addOwnersItem(owner);
 
 		properties.add(currentProperty);
-//		}
 
-//		} catch (Exception ex) {
-//			log.error("Error : " + ex.toString());
-//		}
-//		Property properties = new Property();
-//		= getPropertiesPlainSearch(criteria, requestInfo);
-		// enrichmentService.enrichBoundary(new PropertyRequest(requestInfo,
-		// properties));
 		return properties;
 	}
 
+	/*
+	 * public List<Property> getPropertiesById(PropertyCriteria criteria) throws
+	 * Exception { // String propertyId = (criteria.getPropertyId() ) ? null // :
+	 * criteria.getPropertyId(); String propertyId = criteria.getPropertyId(); //
+	 * String mobileNumber = criteria.getMobileNumber();
+	 * 
+	 * String propertyUrl = "https://mcraipur.in/api/getPropertyDetails?PROP_UID=" +
+	 * propertyId;
+	 * 
+	 * if (propertyId != null) { propertyUrl =
+	 * "https://mcraipur.in/api/getPropertyDetails?PROP_UID=" + propertyId; } else
+	 * if (mobileNumber != null) { propertyUrl =
+	 * "https://apis.mcraipur.in/api/searchPropertyList?MOBILE=" + mobileNumber; }
+	 * 
+	 * 
+	 * List<Property> properties = new ArrayList<>();
+	 * 
+	 * // try { // Map<String, Object> propertySearchResponse =
+	 * restTemplate.getForObject(propertyUrl, Map.class); String response =
+	 * restTemplate.getForObject(propertyUrl, String.class);
+	 * log.info("RMC response : " + response); ObjectMapper objectMapper = new
+	 * ObjectMapper(); Map<String, Object> propertySearchResponse =
+	 * objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {
+	 * });
+	 * 
+	 * Map<String, Object> propertyDetails = ((List<Map<String, Object>>)
+	 * propertySearchResponse .get("GETPROPDETAILSResult")).get(0);
+	 * 
+	 * log.info("RMC propertyDetails: " + propertyDetails.toString()); // Property
+	 * currentProperty = new Property();
+	 * 
+	 * // for (Map<String, Object> propertyDetails : propertyDetailsList) { Object
+	 * area = propertyDetails.get("TOTAL_PLOT_AREA"); Double landArea = 0.0; if
+	 * (area == null) { landArea = null; } else { landArea =
+	 * Double.valueOf(area.toString()); } Object wardNo =
+	 * propertyDetails.get("WARD_NO"); Object wardName =
+	 * propertyDetails.get("WARD_NAME"); Object zoneNO =
+	 * propertyDetails.get("ZONE_NO"); Locality locality =
+	 * Locality.builder().code(wardNo != null ? "RMCLC" + wardNo.toString() : null)
+	 * .name(wardName != null ? wardName.toString() :
+	 * null).label("Locality").build();
+	 * 
+	 * Map<String, Object> addressAdditional = new HashMap<>();
+	 * 
+	 * addressAdditional.put("colonyName", propertyDetails.get("COLONY_NAME"));
+	 * addressAdditional.put("houseNo", propertyDetails.get("HOUSE_NO"));
+	 * addressAdditional.put("wardNo", propertyDetails.get("WARD_NO"));
+	 * addressAdditional.put("address", propertyDetails.get("ADDRESS"));
+	 * addressAdditional.put("zoneNo", propertyDetails.get("ZONE_NO"));
+	 * 
+	 * Address address = Address.builder().city("Raipur").tenantId("cg.raipur")
+	 * .wardNo(wardNo != null ? wardNo.toString() : null).zoneNo(wardNo != null ?
+	 * zoneNO.toString() : null) .locality(locality).build();
+	 * 
+	 * List<String> owners =
+	 * Arrays.asList(propertyDetails.get("PROPERTY_OWNER").toString().split(","));
+	 * String ownershipcategory = owners.size() == 1 ? "INDIVIDUAL.SINGLEOWNER" :
+	 * "INDIVIDUAL.MULTIPLEOWNERS";
+	 * 
+	 * Object coveredArea = propertyDetails.get("TOTAL_COVERED_AREA"); BigDecimal
+	 * superBuiltUpArea = coveredArea != null ? new
+	 * BigDecimal(coveredArea.toString()) : BigDecimal.ZERO; Long noOfFloors =
+	 * propertyDetails.get("TOTAL_FLOORS") != null ? ((Number)
+	 * propertyDetails.get("TOTAL_FLOORS")).longValue() : null;
+	 * 
+	 * Property currentProperty =
+	 * Property.builder().source(Source.fromValue("MUNICIPAL_RECORDS"))
+	 * .ownershipCategory(ownershipcategory).channel(Channel.fromValue("SYSTEM"))
+	 * .superBuiltUpArea(superBuiltUpArea).usageCategory(propertyDetails.get(
+	 * "PROPERTY_USE").toString()) // .propertyType(rs.getString("propertytype"))
+	 * .noOfFloors(noOfFloors) // .auditDetails(auditdetails)
+	 * .landArea(landArea).address(address).propertyId(propertyDetails.get(
+	 * "PROP_UID").toString())
+	 * .tenantId("cg.raipur").status(Status.fromValue("ACTIVE"))
+	 * .dueAmount(propertyDetails.get("DUE_AMOUNT").toString()).build();
+	 * 
+	 * OwnerInfo owner =
+	 * OwnerInfo.builder().status(Status.fromValue("ACTIVE")).tenantId("cg.raipur")
+	 * .mobileNumber(propertyDetails.get("MOBILE").toString())
+	 * .name(propertyDetails.get("PROPERTY_OWNER").toString().split(",")[0]).build()
+	 * ;
+	 * 
+	 * currentProperty.addOwnersItem(owner);
+	 * 
+	 * properties.add(currentProperty); // }
+	 * 
+	 * // } catch (Exception ex) { // log.error("Error : " + ex.toString()); // } //
+	 * Property properties = new Property(); // = getPropertiesPlainSearch(criteria,
+	 * requestInfo); // enrichmentService.enrichBoundary(new
+	 * PropertyRequest(requestInfo, // properties)); return properties; }
+	 */
 }
