31397e8b102b53a4951b76a69e15c5424920d588
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-07-29 16:52:53 +0530

    FSM: property search locality api call add-1

--- municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/pt/web/service/PropertyService.java
@@ -165,7 +165,7 @@ public class PropertyService {
 	}
 
 	private Map<String, Object> locationZoneSearchFromMdms(String tenantId, String wardCode) {
-		String url = "/egov-location/location/v11/boundarys/_search?hierarchyTypeCode=REVENUE&boundaryType=Zone&tenantId="
+		String url = "http://localhost:8091/egov-location/location/v11/boundarys/_search?hierarchyTypeCode=REVENUE&boundaryType=Zone&tenantId="
 				+ tenantId;
 		Map<String, Object> response = restTemplate.getForObject(url, Map.class);
 		List<Map<String, Object>> boundaryList = ((List<Map<String, Object>>) response.get("TenantBoundary")).stream()
