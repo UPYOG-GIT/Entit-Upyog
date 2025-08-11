74f2e434f06f825e30610d3650d98b83029d426b
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-08-11 14:39:52 +0530

    FSM:add more notifications on whatsapp

--- municipal-services/fsm/src/main/java/org/egov/fsm/service/notification/WhatsappNotification.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/service/notification/WhatsappNotification.java
@@ -44,6 +44,14 @@ public class WhatsappNotification {
 			Map<String, Object> requestBody = new HashMap<>();
 			if (status.equals("PENDING_FEE_PAYMENT")) {
 				requestBody = applicationCreatedMessage(citizenName, applicationNo, amount, mobileNumber);
+			} else if (status.equals("ASSIGN_DSO") || status.equals("ASSIGN_DRIVER")) {
+				requestBody = feePaidMessage(citizenName, applicationNo, mobileNumber);
+			} else if (status.equals("PENDING_WORK_START_BY_DRIVER")) {
+				String vehicleNo = fsmRequest.getFsm().getVehicle().getRegistrationNumber();
+				String driverName = fsmRequest.getFsm().getDriver().getName();
+				String driverContNo = fsmRequest.getFsm().getDriver().getOwner().getMobileNumber();
+				requestBody = assignDsoDriverMessage(citizenName, applicationNo, mobileNumber, vehicleNo, driverName,
+						driverContNo);
 			}
 
 			HttpHeaders headers = new HttpHeaders();
@@ -78,6 +86,50 @@ public class WhatsappNotification {
 		templateParams.add(citizenName);
 		templateParams.add(applicationNo);
 		templateParams.add(amount);
+
+		requestBody.put("templateParams", templateParams);
+
+		return requestBody;
+	}
+
+	private Map<String, Object> feePaidMessage(String citizenName, String applicationNo, String mobileNumber) {
+
+		Map<String, Object> requestBody = new HashMap<>();
+
+		requestBody.put("apiKey",
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3YmRjNGIyY2Y5ZmU4MGJmZDAwYzJhMSIsIm5hbWUiOiJOYWdhciBOaWdhbSBSYWlwdXIiLCJhcHBOYW1lIjoiQWlTZW5zeSIsImNsaWVudElkIjoiNjdiZDZjMjNmN2JlN2QwZWZkMWRmNDBjIiwiYWN0aXZlUGxhbiI6Ik5PTkUiLCJpYXQiOjE3NDA0ODk5MDZ9.NBLaWEeCwg9Z3bwvaYrtOarkIRZbIuF7IwqZaqjxyjw");
+		requestBody.put("campaignName", "fsm_fee_paid");
+		requestBody.put("destination", mobileNumber);
+		requestBody.put("userName", "Nagar Nigam Raipur");
+		requestBody.put("source", "new-landing-page form");
+
+		List<Object> templateParams = new ArrayList<>();
+		templateParams.add(citizenName);
+		templateParams.add(applicationNo);
+
+		requestBody.put("templateParams", templateParams);
+
+		return requestBody;
+	}
+
+	private Map<String, Object> assignDsoDriverMessage(String citizenName, String applicationNo, String mobileNumber,
+			String vehicleNo, String driverName, String driverContNo) {
+
+		Map<String, Object> requestBody = new HashMap<>();
+
+		requestBody.put("apiKey",
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3YmRjNGIyY2Y5ZmU4MGJmZDAwYzJhMSIsIm5hbWUiOiJOYWdhciBOaWdhbSBSYWlwdXIiLCJhcHBOYW1lIjoiQWlTZW5zeSIsImNsaWVudElkIjoiNjdiZDZjMjNmN2JlN2QwZWZkMWRmNDBjIiwiYWN0aXZlUGxhbiI6Ik5PTkUiLCJpYXQiOjE3NDA0ODk5MDZ9.NBLaWEeCwg9Z3bwvaYrtOarkIRZbIuF7IwqZaqjxyjw");
+		requestBody.put("campaignName", "fsm_assign_dso");
+		requestBody.put("destination", mobileNumber);
+		requestBody.put("userName", "Nagar Nigam Raipur");
+		requestBody.put("source", "new-landing-page form");
+
+		List<Object> templateParams = new ArrayList<>();
+		templateParams.add(citizenName);
+		templateParams.add(applicationNo);
+		templateParams.add(vehicleNo);
+		templateParams.add(driverName);
+		templateParams.add(driverContNo);
 
 		requestBody.put("templateParams", templateParams);
 
