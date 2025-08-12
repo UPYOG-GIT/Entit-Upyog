296bb1cf93e0a6a0f37368072cdd6a6a9bdd1323
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-08-12 15:47:34 +0530

    User-otp: changes for fsm rmc otp

--- core-services/user-otp/src/main/java/org/egov/web/contract/Otp.java
+++ core-services/user-otp/src/main/java/org/egov/web/contract/Otp.java
@@ -19,6 +19,7 @@ public class Otp {
     private String tenantId;
     private String type;
     private String userType;
+    private String module;
 
     @JsonIgnore
     public OtpRequestType getTypeOrDefault() {
--- core-services/user-otp/src/main/java/org/egov/web/contract/OtpRequest.java
+++ core-services/user-otp/src/main/java/org/egov/web/contract/OtpRequest.java
@@ -18,6 +18,7 @@ public class OtpRequest {
                 .tenantId(getTenantId())
                 .type(getType())
                 .userType(getUserType())
+                .module(getModule())
                 .build();
     }
 
@@ -35,6 +36,10 @@ public class OtpRequest {
 
     private String getTenantId() {
         return otp != null ? otp.getTenantId() : null;
+    }
+    
+    private String getModule() {
+    	return otp != null ? otp.getModule() : null;
     }
 }
 
