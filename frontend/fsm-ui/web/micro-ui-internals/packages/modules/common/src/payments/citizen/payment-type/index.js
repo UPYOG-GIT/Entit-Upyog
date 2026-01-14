ddb4e8a0db63043626ea1a36b71fa969f0e59d13
EntitDev4 <dev4entitcs@gmail.com>
2026-01-14 14:35:22 +0530

    changes for call back url

--- frontend/fsm-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
+++ frontend/fsm-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
@@ -84,10 +84,10 @@ export const SelectPaymentType = (props) => {
         }, 
         // success below url generation is commented according to requirement if not worked then
         //  just uncommend the 87,88,89 line number and comment the 90 line number
-        callbackUrl: window.location.href.includes("mcollect")
-          ? `${window.location.protocol}//${window.location.host}/fsm-ui/citizen/payment/success/${businessService}/${consumerCode}/${tenantId}?workflow=mcollect`
-          : `${window.location.protocol}//${window.location.host}/fsm-ui/citizen/payment/success/${businessService}/${consumerCode}/${tenantId}`,
-        // callbackUrl: `${window.location.protocol}//${window.location.host}/fsm-ui/citizen/payment/success/${businessService}/${consumerCode}/${tenantId}?propertyId=${consumerCode}`,
+        // callbackUrl: window.location.href.includes("mcollect")
+        //   ? `${window.location.protocol}//${window.location.host}/fsm-ui/citizen/payment/success/${businessService}/${consumerCode}/${tenantId}?workflow=mcollect`
+        //   : `${window.location.protocol}//${window.location.host}/fsm-ui/citizen/payment/success/${businessService}/${consumerCode}/${tenantId}`,
+        callbackUrl: `${window.location.protocol}//${window.location.host}/fsm-ui/citizen/payment/success/${businessService}/${consumerCode}/${tenantId}?propertyId=${consumerCode}`,
           additionalDetails: {
           isWhatsapp: false,
         },
@@ -104,6 +104,7 @@ export const SelectPaymentType = (props) => {
       if (d?.paymentType == "RAZORPAY") {
         // console.log("data :" + JSON.stringify(data));
 
+        console.log("CallbackUrl", JSON.stringify(data?.Transaction?.callbackUrl));
         // window.location = redirectUrl;
         // const raw = redirectUrl.split("data=")[1];
         // const decoded = JSON.parse(decodeURIComponent(raw));
